a96377f1372ab2a7a29af0f687ae3e5bbda6634e
Jordon Bedwell <envygeeks@gmail.com>
2014-08-20 02:29:23 -0500

    Drop the _

--- spec/rspec/helper.rb
+++ spec/rspec/helper.rb
@@ -1,11 +1,14 @@
-db_cmd = ENV["DB_TYPE"] == "mysql2" ? "mysql -u %s -e" : "psql -U %s -c"
-db_drop_cmd = "'DROP DATABASE IF EXISTS %s' >/dev/null 2>&1"
-db_create_cmd = "'CREATE DATABASE %s' >/dev/null 2>&1"
-db_database = "active_record_mocks_testing"
-db_user = ENV["DB_USER"] || "jordon"
+ENV["DBHOST"] ||= "localhost"
+ENV["DBNAME"] ||= "active_record_mocks_testing"
+ENV["DBTYPE"] ||= "postgresql"
+ENV["DBUSER"] ||= "jordon"
+
+db_drop_cmd = "'DROP DATABASE IF EXISTS %s;' >/dev/null 2>&1"
+db_cmd = ENV["DBTYPE"] == "mysql2" ? "mysql -u %s -e" : "psql -U %s -c"
+db_create_cmd = "'CREATE DATABASE %s;' >/dev/null 2>&1"
 
 if RbConfig::CONFIG["ruby_install_name"] == "jruby"
-  ENV["DB_TYPE"] = ENV["DB_TYPE"] ==
+  ENV["DBTYPE"] = ENV["DBTYPE"] ==
     "mysql2" ? "jdbcmysql" : "jdbcpostgresql"
 end
 
@@ -16,25 +19,25 @@ require "rspec/expect_error"
 require "pry" unless ENV["CI"]
 
 ActiveRecord::Base.establish_connection(
-  :password => ENV["DB_PASSWORD"],
-  :database => db_database,
-  :username => db_user,
-  :host     => "localhost",
-  :adapter  => ENV["DB_TYPE"] || "postgresql"
+  :username => ENV["DBUSER"],
+  :password => ENV["DBPASSWORD"],
+  :database => ENV["DBNAME"],
+  :host     => ENV["DBHOST"],
+  :adapter  => ENV["DBTYPE"]
 )
 
 Dir[File.expand_path("../../support/**/*.rb", __FILE__)].each do |f|
   require f
 end
 
-RSpec.configure do |config|
-  config.before :suite do
-    system db_cmd % db_user + db_drop_cmd % db_database
-    system db_cmd % db_user + db_create_cmd % db_database
+RSpec.configure do |c|
+  c.before :suite do
+    system db_cmd % ENV["DBUSER"] + db_drop_cmd   % ENV["DBNAME"]
+    system db_cmd % ENV["DBUSER"] + db_create_cmd % ENV["DBNAME"]
   end
 
-  config.after :suite do
+  c.after :suite do
     ActiveRecord::Base.connection.disconnect!
-    system db_cmd % db_user + db_drop_cmd % db_database
+    system db_cmd % ENV["DBUSER"] + db_drop_cmd % ENV["DBNAME"]
   end
 end
